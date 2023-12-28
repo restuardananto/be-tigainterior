deb25db3869d0b658baab699360c49daa35d9d37
restuardananto <ini_restu@outlook.com>
2023-12-29 00:41:53 +0800

    Hapus semua file dalam repositori

--- config/Database.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const db = new Sequelize("tigainterior", "restu", "restu", {
-  dialect: "mysql",
-  host: "localhost",
-  timezone: "+07:00",
-});
-
-export default db;
